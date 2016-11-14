f7f78a946294b6a6c02141c97f0270abedea2048
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2016-11-14 21:26:29 -0200

    Corrigindo links da pagina de instalacao e ajustes da pagina de opcoes

---
+++ Azkfile.js
@@ -0,0 +1,32 @@
+/**
+ * Documentation: http://docs.azk.io/Azkfile.js
+ */
+// Adds the systems that shape your system
+systems({
+  'skitter-site': {
+    depends: [],
+    image: {"docker": "azukiapp/node"},
+    provision: [
+      "npm install",
+    ],
+    workdir: "/azk/#{manifest.dir}",
+    shell: "/bin/bash",
+    command: ["npm", "start"],
+    wait: 20,
+    mounts: {
+      '/azk/#{manifest.dir}': sync("."),
+      '/azk/#{manifest.dir}/node_modules': persistent("./node_modules"),
+    },
+    scalable: {"default": 1},
+    http: {
+      domains: [ "#{system.name}.#{azk.default_domain}" ]
+    },
+    ports: {
+      http: "3000/tcp",
+    },
+    envs: {
+      NODE_ENV: "dev",
+      PORT: "3000",
+    },
+  },
+});
